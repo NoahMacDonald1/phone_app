e3fa9b609419846311898b9157c28e53c76730f7
NoahMacDonald1 <78161150+NoahMacDonald1@users.noreply.github.com>
2021-04-02 02:08:18 -0400

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -6,15 +6,15 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-int buttonCnt = 0;
+int buttonCnt = 0; // Button counter variable
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     public void button_pushed(View view) {
-        buttonCnt++;
-        TextView quantityTextView = (TextView) findViewById(R.id.after_push);
-        quantityTextView.setText("Button pushed: " + buttonCnt + " times.");
+        buttonCnt++; // iterate the counter
+        TextView quantityTextView = (TextView) findViewById(R.id.after_push); // display the TextView after_push once the button is pressed
+        quantityTextView.setText("Button pushed: " + buttonCnt + " times."); // Update the text in after_push
     }
 }
